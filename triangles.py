827f01a99bedaa74e7814c4f07d4c4c5a4b0b3d1
Your Name <rutaslv@hotmail.lt>
2020-06-29 23:41:29 +0300

    assertion wrapped into try - except - else

--- triangles.py
+++ triangles.py
@@ -4,20 +4,20 @@
   ####
  55555   '''
 
-userInput = float(input("Enter a number between 1 and 10: "))
-
-assert userInput.is_integer() and 1 <= userInput <= 10, "Invalid number."
-
-intNumber = int(userInput)
-
-spaces = intNumber
-
-for row in range(intNumber):
-    spaces -= 1
-    for column in range(spaces): print(" ", end="")
-    for column in range(intNumber - spaces):
-        if (row + 1) % 2 == 1:
-            print(row + 1, end="")
-        else:
-            print("#", end="")
-    print("")
+try:
+    userInput = float(input("Enter a number between 1 and 10: "))
+    assert userInput.is_integer() and 1 <= userInput <= 10, "Invalid number."
+except AssertionError as object:
+    print(object)
+else:
+    intNumber = int(userInput)
+    spaces = intNumber
+    for row in range(intNumber):
+        spaces -= 1
+        for column in range(spaces): print(" ", end="")
+        for column in range(intNumber - spaces):
+            if (row + 1) % 2 == 1:
+                print(row + 1, end="")
+            else:
+                print("#", end="")
+        print("")
